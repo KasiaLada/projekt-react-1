8875c220b2dc7612976867135dfb74237659a2ae
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2023-07-05 14:48:55 +0200

    first component

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import './Navigation.css';
+
+
+const App = () => {
+	return (
+<header>
+  <div className="container">
+    <div className = "navigation">
+      <p>
+        moja firma
+      </p>
+    </div>
+  </div>
+</header>
+  );
+};
+
+export default App;
