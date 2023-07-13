4b1533cace6baa9282980e3065ba64e8b70e54d0
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2023-07-13 20:27:30 +0200

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navigation from './components/Navigation';
 import Header from './components/Header';
 import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
-
 import Offer from './components/Offer';
 
 const App = () => {
